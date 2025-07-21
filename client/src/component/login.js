838442f94411076483d6160fc49e52f429e0dd9d
RinkiShara <rinki2001sharma@gmail.com>
2025-07-21 22:12:42 +0530

    add signup  in a form '

--- client/src/component/login.js
+++ client/src/component/login.js
@@ -1,39 +1,67 @@
-import React from "react";
+import React, { useState } from "react";
 import graph from "../assets/graph.webp";
 import "../style/style.css";
-import { FaChartLine } from 'react-icons/fa';
+import { FaChartLine } from "react-icons/fa";
 
-function login() {
+function Login() {
+  const [isSignup, setIsSignup] = useState(false);
+
+  const toggleForm = () => setIsSignup(!isSignup);
+
   return (
     <div className="main_container">
       <div className="left_container">
         <div className="heading">
           <h1>Expense Tracker</h1>
         </div>
+
         <div className="form_container">
           <div className="para_heading">
-            Welcome Back
-            <p>Please enter your details to log in </p>
+            {isSignup ? "Create an Account" : "Welcome Back"}
+            <p>
+              {isSignup
+                ? "Please fill in your details to sign up"
+                : "Please enter your details to log in"}
+            </p>
+
             <form className="form_container">
+              {isSignup && (
+                <>
+                  <label>Full Name</label>
+                  <div className="input_field">
+                    <input type="text" placeholder="John Doe" />
+                  </div>
+                </>
+              )}
+
               <label>Email Address</label>
               <div className="input_field">
                 <input type="email" placeholder="john@gmail.com" />
               </div>
+
               <label>Password</label>
               <div className="input_field">
-                <input type="email" placeholder="Min 8 charaters" />
+                <input type="password" placeholder="Min 8 characters" />
               </div>
-              <button>Login</button>
+
+              <button type="submit">{isSignup ? "Sign Up" : "Login"}</button>
+
               <p>
-                Don't have an Acount ? <span>Sign up</span>
+                {isSignup
+                  ? "Already have an account?"
+                  : "Don't have an account?"}{" "}
+                <span onClick={toggleForm}>
+                  {isSignup ? "Login" : "Sign up"}
+                </span>
               </p>
             </form>
           </div>
         </div>
       </div>
+
       <div className="right_container">
         <div className="blue_box">
-          <div className="white_box ">
+          <div className="white_box">
             <div className="icon_circle">
               <FaChartLine className="icon" />
             </div>
@@ -50,4 +78,4 @@ function login() {
   );
 }
 
-export default login;
+export default Login;
