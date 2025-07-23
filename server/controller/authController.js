82f2720969b66515dfbee7c95a423f779dac205f
RinkiShara <rinki2001sharma@gmail.com>
2025-07-24 00:12:03 +0530

    get user by id api

--- server/controller/authController.js
+++ server/controller/authController.js
@@ -73,10 +73,34 @@ exports.loginUser = async (req, res) => {
         name: user.name,
         email: user.email,
         avatar: user.avatar,
+        token: token
       },
     });
   } catch (error) {
     console.error("Login error:", error.message);
     res.status(500).json({ message: "Server error" });
+  }
+};
+
+// Get user by ID
+exports.getUserById = async (req, res) => {
+  try {
+    const userId = req.params.id; 
+    if (!userId) {
+      return res.status(400).json({ message: "User ID is required" });
+    } 
+    const user = await User.findById(userId);
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    } 
+    res.status(200).json({
+      id: user._id,
+      name: user.name,
+      email: user.email,
+      avatar: user.avatar,
+    }); 
+  } catch (error) {
+    res.status(500).json({ message: "Server error" });
+    console.error("Get user error:", error.message);
   }
 };
--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const router = express.Router();
-const { registerUser, loginUser } = require("../controller/authController");
+const { registerUser, loginUser ,getUserById} = require("../controller/authController");
 const upload = require("../middleware/multer"); 
+const authenticateUser = require("../middleware/projected");
 
 router.post("/register", upload.single("avatar"), registerUser);
 router.post("/login", loginUser);
+router.get('/getUser/:id',authenticateUser,getUserById)
 
 module.exports = router;
