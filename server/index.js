8ec738698bc9d3045d7dd37b6ff524815f8d08d5
RinkiShara <rinki2001sharma@gmail.com>
2025-08-09 16:01:47 +0530

    live api edid on axios

--- server/index.js
+++ server/index.js
@@ -1,25 +1,34 @@
-const express = require('express');
-const cors = require('cors');
-const dotenv = require('dotenv'); 
-const connectDB = require('./config/db');
+const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv");
+const connectDB = require("./config/db");
 
-dotenv.config(); 
+dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Connect to MongoDB
 connectDB();
-app.use(cors());
+app.use(
+  cors({
+    origin: [
+      "http://localhost:3000",
+      "https://expense-tracker-oicsars44-rinki2400s-projects.vercel.app",
+    ],
+
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use("/uploads/avatars", express.static("uploads/avatars"));
 
-app.use('/api/auth', require('./routes/authRoutes'))
-app.use('/api/income', require('./routes/IncomeRoutes'))
-app.use('/api/expense', require('./routes/ExpenseRoutes'))
-app.use('/api/dashboard', require('./routes/DashboardRoutes'))
-app.get('/', (req, res) => res.send('Server is running'));
+app.use("/api/auth", require("./routes/authRoutes"));
+app.use("/api/income", require("./routes/IncomeRoutes"));
+app.use("/api/expense", require("./routes/ExpenseRoutes"));
+app.use("/api/dashboard", require("./routes/DashboardRoutes"));
+app.get("/", (req, res) => res.send("Server is running"));
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
-
