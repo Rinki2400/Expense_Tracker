188fed78539c72798580beebf2f018251aa7a371
RinkiShara <rinki2001sharma@gmail.com>
2025-11-13 17:07:21 +0530

    remove comment code

--- client/src/component/Dashboard/Dashboard.js
+++ client/src/component/Dashboard/Dashboard.js
@@ -1,29 +1,4 @@
-// import React from 'react';
-// import { Outlet } from 'react-router-dom';
-// import '../../style/Dashboard.css';
-// import Navbar from './Navbar';
-// import SideBar from './SideBar';
 
-
-// function Dashboard() {
-//   return (
-//     <>
-    
-   
-//     <div className="dashboard_container">
-//       <Navbar />
-//       <div className="dashboard_main">
-//         <SideBar />
-//         <div className="right_container">
-//         <Outlet/>
-//         </div>
-//       </div>
-//     </div>
-//      </>
-//   );
-// }
-
-// export default Dashboard;
 import React from 'react';
 import { Outlet } from 'react-router-dom';
 import '../../style/Dashboard.css';
